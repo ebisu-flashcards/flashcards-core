96c17dd7cc4794679a8b1688e59a147b1ad6b473
ZanSara <sarazanzo94@gmail.com>
2021-07-15 22:15:34 +0200

    Add flake8 pre-commit hook

--- setup.py
+++ setup.py
@@ -10,53 +10,55 @@ from setuptools import setup, find_packages
 
 
 HERE = Path(__file__).parent.absolute()
-with (HERE / 'README.md').open('rt') as fh:
+with (HERE / "README.md").open("rt") as fh:
     LONG_DESCRIPTION = fh.read().strip()
 
 
 REQUIREMENTS: dict = {
-    'core': [
+    "core": [
         "sqlalchemy",
         "ebisu",
     ],
-    'test': [
+    "test": [
         "pytest",
         "pytest-cov",
         "pytest-random-order",
+        "pre-commit",
+        "black",
+        "flake8",
     ],
-    'doc': [
-        'sphinx',
+    "doc": [
+        "sphinx",
     ],
 }
 
 
 setup(
-    name='flashcards-core',
+    name="flashcards-core",
     version="0.0.1",
-
-    author='Sara Zanzottera',
-    author_email='',
-    description='Flashcards Core',
+    author="Sara Zanzottera",
+    author_email="",
+    description="Flashcards Core",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    url='',
-
+    long_description_content_type="text/markdown",
+    url="",
     packages=find_packages(),
-    python_requires='>=3.6, <4',
+    python_requires=">=3.6, <4",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-
-    install_requires=REQUIREMENTS['core'],
+    install_requires=REQUIREMENTS["core"],
     extras_require={
         **REQUIREMENTS,
         # The 'dev' extra is the union of 'test' and 'doc', with an option
         # to have explicit development dependencies listed.
-        'dev': [req
-                for extra in ['dev', 'test', 'doc']
-                for req in REQUIREMENTS.get(extra, [])],
+        "dev": [
+            req
+            for extra in ["dev", "test", "doc"]
+            for req in REQUIREMENTS.get(extra, [])
+        ],
         # The 'all' extra is the union of all requirements.
-        'all': [req for reqs in REQUIREMENTS.values() for req in reqs],
-    }
+        "all": [req for reqs in REQUIREMENTS.values() for req in reqs],
+    },
 )
