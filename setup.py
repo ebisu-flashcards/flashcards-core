e7e9cb9fa0593e394df78bc12eb72292967bd199
ZanSara <sarazanzo94@gmail.com>
2021-07-26 11:42:02 +0200

    Simplify setup.py and add .vscode to gitignore

--- setup.py
+++ setup.py
@@ -1,38 +1,9 @@
-"""
-setup.py for flashcards-core.
-
-For reference see
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-
-"""
 from pathlib import Path
 from setuptools import setup, find_packages
-
 
 HERE = Path(__file__).parent.absolute()
 with (HERE / "README.md").open("rt") as fh:
     LONG_DESCRIPTION = fh.read().strip()
-
-
-REQUIREMENTS: dict = {
-    "core": [
-        "sqlalchemy",
-        "ebisu",
-    ],
-    "test": [
-        "pytest",
-        "pytest-cov",
-        "pytest-random-order",
-        "pre-commit",
-        "black",
-        "flake8",
-        "coveralls",  # To publish the coverage data on coveralls
-    ],
-    "doc": [
-        "sphinx",
-    ],
-}
-
 
 setup(
     name="flashcards-core",
@@ -49,17 +20,20 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    install_requires=REQUIREMENTS["core"],
+    install_requires=[
+        "sqlalchemy",
+        "ebisu",
+    ],
     extras_require={
-        **REQUIREMENTS,
-        # The 'dev' extra is the union of 'test' and 'doc', with an option
-        # to have explicit development dependencies listed.
         "dev": [
-            req
-            for extra in ["dev", "test", "doc"]
-            for req in REQUIREMENTS.get(extra, [])
-        ],
-        # The 'all' extra is the union of all requirements.
-        "all": [req for reqs in REQUIREMENTS.values() for req in reqs],
+            "pytest",
+            "pytest-cov",
+            "pytest-random-order",
+            "pre-commit",
+            "black",
+            "flake8",
+            "coveralls",  # To publish the coverage data on coveralls
+            "sphinx",
+        ]
     },
 )
