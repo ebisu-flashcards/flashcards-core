8d4b82ab62eff14f48ee78d549c13f1b5f77f6f6
Sara Zan <ZanSara@users.noreply.github.com>
2021-07-29 16:17:04 +0200

    Add reference to the pip issue

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 
 # This file enables editable installs (pip install -e .[dev])
 
+# ##########################################################
+#  See this issue: https://github.com/pypa/pip/issues/7953
 import site
 import sys
 site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+# ##########################################################
 
 import setuptools
 
