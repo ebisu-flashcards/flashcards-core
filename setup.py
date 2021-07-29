b1d6b757ebda490258a323e0c14b76bf5cd7c695
Sara Zan <ZanSara@users.noreply.github.com>
2021-07-29 16:11:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 
 # This file enables editable installs (pip install -e .[dev])
 
+import site
+import sys
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
 import setuptools
 
 if __name__ == "__main__":
