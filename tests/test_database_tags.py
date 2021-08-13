be221254b7d7a1f453dd0c3d9d0f0e44ed0adb0c
ZanSara <sarazanzo94@gmail.com>
2021-08-14 00:07:23 +0200

    Add test for get_by_name

--- tests/test_database_tags.py
+++ tests/test_database_tags.py
@@ -5,6 +5,15 @@ def test_tag_create(session):
     assert Tag.create(session=session, name="test")
 
 
+def test_tag_get_by_name_exists(session):
+    tag = Tag.create(session=session, name="test")
+    assert tag == Tag.get_by_name(session=session, name="test")
+
+
+def test_tag_get_by_name_doesnt_exist(session):
+    assert not Tag.get_by_name(session=session, name="test")
+
+
 def test_tag_repr(session):
     tag = Tag.create(session=session, name="tag")
     assert f"<Tag 'tag' (ID: {tag.id})>" == f"{tag}"
