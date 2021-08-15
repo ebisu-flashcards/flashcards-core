b93c2fb9f434b8a1693777702fd969d2a7ead0e2
ZanSara <sarazanzo94@gmail.com>
2021-08-15 20:36:29 +0200

    Update and fix the importer to match the exporter

--- flashcards_core/database/importer.py
+++ flashcards_core/database/importer.py
@@ -1,6 +1,6 @@
 from typing import Any, Mapping
 
-# import json
+import json
 import logging
 from uuid import UUID
 
@@ -9,22 +9,24 @@ from sqlalchemy.exc import IntegrityError
 from flashcards_core.database import Base
 
 
-# def import_from_json(session: Session, json_string: str, **json_kwargs) -> None:
-#     """
-#     Import the objects from their JSON representation.
-#     Simple wrapper around `import_from_dict()`.
+def import_from_json(session: Session, json_string: str, **json_kwargs) -> None:
+    """
+    Import the objects from their JSON representation.
+    Simple wrapper around `import_from_dict()`.
 
-#     :param session: the session (see flashcards_core.database:init_session()).
-#     :param json_string: the string containing the data of the objects to import.
-#         It should have been created with `export_to_json()`
-#         or match the same schema.
-#     :param json_kwargs: any parameter you may wish to pass to `json.loads()`
-#     """
-#     hierarchy = json.loads(json_string, **json_kwargs)
-#     return import_from_dict(session=session, hierarchy=hierarchy)
+    :param session: the session (see flashcards_core.database:init_session()).
+    :param json_string: the string containing the data of the objects to import.
+        It should have been created with `export_to_json()`
+        or match the same schema.
+    :param json_kwargs: any parameter you may wish to pass to `json.loads()`
+    """
+    hierarchy = json.loads(json_string, **json_kwargs)
+    return import_from_dict(session=session, hierarchy=hierarchy)
 
 
-def import_from_dict(session: Session, hierarchy: Mapping[str, Any]) -> None:
+def import_from_dict(  # noqa: C901
+    session: Session, hierarchy: Mapping[str, Any], stop_on_error=False
+) -> None:
     """
     Create objects in the database from the data contained in the dictionary.
     Note that the keys must be strings, not UUID objects.
@@ -89,6 +91,8 @@ def import_from_dict(session: Session, hierarchy: Mapping[str, Any]) -> None:
     :param session: the session (see flashcards_core.database:init_session()).
     :param hierarchy: a dictionary containing all the data of the objects to import.
         See above or `export_to_dict()` for more info.
+    :param stop_on_error: if an Integrity error is raised, stop instead of
+        skipping the object.
     :returns: None
     """
     for tablename, entities in hierarchy.items():
@@ -96,22 +100,52 @@ def import_from_dict(session: Session, hierarchy: Mapping[str, Any]) -> None:
         # Find the relevant table
         table = Base.metadata.tables.get(tablename)
         if table is None:
-            raise ValueError(
+            message = (
                 "The hierarchy contains a key that does not "
                 f"correspond to any know table: '{tablename}'. "
-                "Remove it and try again."
             )
+            if stop_on_error:
+                raise ValueError(message)
+            else:
+                logging.error(message)
+                continue
+
         logging.debug(f"Importing into {tablename}...")
 
-        for index, values in entities.items():
-            try:
-                logging.debug(f"Importing {index}: {values}")
-                uuid = UUID(index)
-                insert = table.insert().values(id=uuid, **values)
-                session.execute(insert)
-            except IntegrityError:
-                logging.error(
-                    f"Cannot import object with id {index} in table "
-                    f"{tablename}: the object either exists already "
-                    "in this database, or it's malformed."
+        if isinstance(entities, dict):
+            for index, values in entities.items():
+                try:
+                    logging.debug(f"Importing {index}: {values}")
+                    uuid = UUID(index)
+                    insert = table.insert().values(id=uuid, **values)
+                    session.execute(insert)
+                except IntegrityError as e:
+                    if stop_on_error:
+                        raise e
+                    logging.error(
+                        f"Cannot import object with id {index} in table "
+                        f"{tablename}: the object either exists already "
+                        "in this database, or it's malformed."
+                    )
+
+        elif isinstance(entities, list):
+            for values in entities:
+                try:
+                    logging.debug(f"Importing {values}")
+                    insert = table.insert().values(values)
+                    session.execute(insert)
+                except IntegrityError as e:
+                    if stop_on_error:
+                        raise e
+                    logging.error(
+                        f"Cannot import row '{values}' in table "
+                        f"{tablename}: the object either exists already "
+                        "in this database, or it's malformed."
+                    )
+        else:
+            if stop_on_error:
+                raise ValueError(
+                    f"Table '{tablename}' is malformed: "
+                    "it's neither a dict nor a list."
                 )
+            logging.error(f"Table '{tablename}' is malformed. Skipping")
--- tests/test_database_importer.py
+++ tests/test_database_importer.py
@@ -1,75 +1,165 @@
 # import json
 import pytest
+import datetime
+from sqlalchemy.exc import IntegrityError
 
-# from freezegun import freeze_time
-
-from flashcards_core.database import Deck  # , Card, Fact, Review, Tag
+from flashcards_core.database import Deck, Card, Fact, Review, Tag
 from flashcards_core.database.importer import import_from_dict  # , import_from_json
 
 
-def test_import(session):
-    deck = {
+def test_import_one_object(session):
+    hierarchy = {
         "decks": {
-            "d852834b-ff4f-4032-9e83-c46cb9989861": {
+            "d852834bff4f40329e83c46cb9989861": {
                 "algorithm": "random",
                 "state": {},
                 "description": "A long description for deck",
                 "name": "Test",
+                "parameters": {},
+            }
+        }
+    }
+    import_from_dict(session=session, hierarchy=hierarchy, stop_on_error=True)
+    assert len(Deck.get_all(session=session)) == 1
+    assert Deck.get_all(session=session)[0].id.hex == "d852834bff4f40329e83c46cb9989861"
+
+
+def test_import_one_association_table(session):
+    hierarchy = {
+        "facttags": [
+            ("d852834bff4f40329e83c46cb9989863", "d852834bff4f40329e83c46cb9989867")
+        ],
+    }
+    import_from_dict(session=session, hierarchy=hierarchy, stop_on_error=True)
+
+
+def test_import_twice_same_object(session):
+    hierarchy = {
+        "decks": {
+            "d852834bff4f40329e83c46cb9989861": {
+                "algorithm": "random",
+                "state": {},
+                "description": "A long description for deck",
+                "name": "Test",
+                "parameters": {},
+            }
+        }
+    }
+    import_from_dict(session=session, hierarchy=hierarchy)
+    import_from_dict(session=session, hierarchy=hierarchy)
+    assert len(Deck.get_all(session=session)) == 1
+    assert Deck.get_all(session=session)[0].id.hex == "d852834bff4f40329e83c46cb9989861"
+    with pytest.raises(IntegrityError):
+        import_from_dict(session=session, hierarchy=hierarchy, stop_on_error=True)
+
+
+def test_import_twice_same_association_table(session):
+    hierarchy = {
+        "facttags": [
+            ("d852834bff4f40329e83c46cb9989863", "d852834bff4f40329e83c46cb9989867")
+        ],
+    }
+    import_from_dict(session=session, hierarchy=hierarchy)
+    import_from_dict(session=session, hierarchy=hierarchy)
+    with pytest.raises(IntegrityError):
+        import_from_dict(session=session, hierarchy=hierarchy, stop_on_error=True)
+
+
+def test_import_wrong_tablename(session):
+    hierarchy = {
+        "wrong": {
+            "d852834bff4f40329e83c46cb9989861": {
+                "algorithm": "random",
+                "state": {},
+                "description": "A long description for deck",
+                "name": "Test",
+                "parameters": {},
+            }
+        }
+    }
+    import_from_dict(session=session, hierarchy=hierarchy)
+    with pytest.raises(ValueError):
+        import_from_dict(session=session, hierarchy=hierarchy, stop_on_error=True)
+
+
+def test_import_malformed_object(session):
+    hierarchy = {"wrong": "value"}
+    import_from_dict(session=session, hierarchy=hierarchy)
+    with pytest.raises(ValueError):
+        import_from_dict(session=session, hierarchy=hierarchy, stop_on_error=True)
+
+
+def test_import_full_hierarchy(session):
+    hierarchy = {
+        "decks": {
+            "d852834bff4f40329e83c46cb9989861": {
+                "name": "Test",
+                "description": "A long description for deck",
+                "algorithm": "random",
+                "state": {},
                 "parameters": {},
             }
         },
         "cards": {
-            "d852834b-ff4f-4032-9e83-c46cb9989862": {
-                "deck_id": "d852834b-ff4f-4032-9e83-c46cb9989861",
-                "question_id": "d852834b-ff4f-4032-9e83-c46cb9989863",
-                "answer_id": "d852834b-ff4f-4032-9e83-c46cb9989864",
+            "d852834bff4f40329e83c46cb9989862": {
+                "deck_id": "d852834bff4f40329e83c46cb9989861",
+                "question_id": "d852834bff4f40329e83c46cb9989863",
+                "answer_id": "d852834bff4f40329e83c46cb9989864",
             }
         },
         "facts": {
-            "d852834b-ff4f-4032-9e83-c46cb9989863": {
-                "format": "plaintext",
-                "value": "a question",
+            "d852834bff4f40329e83c46cb9989863": {"format": "text", "value": "question"},
+            "d852834bff4f40329e83c46cb9989864": {"format": "text", "value": "answer"},
+        },
+        "reviews": {
+            "d852834bff4f40329e83c46cb9989865": {
+                "algorithm": "random",
+                "card_id": "d852834bff4f40329e83c46cb9989862",
+                "datetime": datetime.datetime(2021, 1, 1, 12, 00, 00, 000000),
+                "result": "1",
             },
-            "d852834b-ff4f-4032-9e83-c46cb9989864": {
-                "format": "plaintext",
-                "value": "an answer",
+            "d852834bff4f40329e83c46cb9989866": {
+                "algorithm": "random",
+                "card_id": "d852834bff4f40329e83c46cb9989862",
+                "datetime": datetime.datetime(2021, 1, 1, 12, 00, 00, 000000),
+                "result": "0",
             },
         },
+        "tags": {
+            "d852834bff4f40329e83c46cb9989867": {"name": "testtag1"},
+            "d852834bff4f40329e83c46cb9989868": {"name": "testtag2"},
+        },
+        "facttags": [
+            ("d852834bff4f40329e83c46cb9989863", "d852834bff4f40329e83c46cb9989867")
+        ],
+        "cardtags": [
+            ("d852834bff4f40329e83c46cb9989862", "d852834bff4f40329e83c46cb9989868")
+        ],
+        "decktags": [
+            ("d852834bff4f40329e83c46cb9989861", "d852834bff4f40329e83c46cb9989867")
+        ],
     }
-    import_from_dict(session=session, hierarchy=deck)
+    import_from_dict(session=session, hierarchy=hierarchy, stop_on_error=True)
+
+    assert Deck.get_all(session=session)[0].id.hex == "d852834bff4f40329e83c46cb9989861"
+    assert Card.get_all(session=session)[0].id.hex == "d852834bff4f40329e83c46cb9989862"
+    assert Fact.get_all(session=session)[0].id.hex == "d852834bff4f40329e83c46cb9989863"
+    assert Fact.get_all(session=session)[1].id.hex == "d852834bff4f40329e83c46cb9989864"
     assert (
-        str(Deck.get_all(session=session)[0])
-        == "<Deck 'Test' (ID: d852834b-ff4f-4032-9e83-c46cb9989861)>"
+        Review.get_all(session=session)[0].id.hex == "d852834bff4f40329e83c46cb9989865"
     )
+    assert (
+        Review.get_all(session=session)[1].id.hex == "d852834bff4f40329e83c46cb9989866"
+    )
+    assert Tag.get_all(session=session)[0].id.hex == "d852834bff4f40329e83c46cb9989867"
+    assert Tag.get_all(session=session)[1].id.hex == "d852834bff4f40329e83c46cb9989868"
 
-
-def test_import_twice_same_object(session):
-    deck = {
-        "decks": {
-            "d852834b-ff4f-4032-9e83-c46cb9989861": {
-                "algorithm": "random",
-                "state": {},
-                "description": "A long description for deck",
-                "name": "Test",
-                "parameters": {},
-            }
-        }
-    }
-    import_from_dict(session=session, hierarchy=deck)
-    import_from_dict(session=session, hierarchy=deck)
-
-
-def test_import_wrong_tablename(session):
-    deck = {
-        "wrong": {
-            "d852834b-ff4f-4032-9e83-c46cb9989861": {
-                "algorithm": "random",
-                "state": {},
-                "description": "A long description for deck",
-                "name": "Test",
-                "parameters": {},
-            }
-        }
-    }
-    with pytest.raises(ValueError):
-        import_from_dict(session=session, hierarchy=deck)
+    assert ["d852834bff4f40329e83c46cb9989867"] == [
+        tag.id.hex for tag in Deck.get_all(session=session)[0].tags
+    ]
+    assert ["d852834bff4f40329e83c46cb9989868"] == [
+        tag.id.hex for tag in Card.get_all(session=session)[0].tags
+    ]
+    assert ["d852834bff4f40329e83c46cb9989867"] == [
+        tag.id.hex for tag in Fact.get_all(session=session)[0].tags
+    ]
