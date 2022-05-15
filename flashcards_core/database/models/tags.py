fdf7b37e8415932a409d97d22ad519dc53b6d4de
ZanSara <sarazanzo94@gmail.com>
2022-05-15 12:58:43 +0200

    Rename primary key of Tag

--- flashcards_core/database/models/tags.py
+++ flashcards_core/database/models/tags.py
@@ -7,7 +7,7 @@ class Tag(Base, CrudOperations):
     __tablename__ = "tags"
 
     #: The name of the tag (primary key)
-    name = Column(String,  primary_key=True, index=True, nullable=False)
+    id = Column(String,  primary_key=True, index=True, nullable=False)
 
     def __repr__(self):
-        return f"<Tag '{self.name}'>"
+        return f"<Tag '{self.id}'>"
