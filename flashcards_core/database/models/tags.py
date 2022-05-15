f2cdf33a84997967c2b7b95bd666753ca38447f3
ZanSara <sarazanzo94@gmail.com>
2022-05-15 13:28:04 +0200

    Reintroduce id for Tag, with note

--- flashcards_core/database/models/tags.py
+++ flashcards_core/database/models/tags.py
@@ -1,4 +1,10 @@
-from sqlalchemy import Column, String
+from typing import Any, Optional
+
+from uuid import uuid4
+from sqlalchemy import Column, String, select
+from sqlalchemy.orm import Session
+
+from flashcards_core.guid import GUID
 from flashcards_core.database import Base
 from flashcards_core.database.crud import CrudOperations
 
@@ -6,8 +12,35 @@ from flashcards_core.database.crud import CrudOperations
 class Tag(Base, CrudOperations):
     __tablename__ = "tags"
 
-    #: The name of the tag (primary key)
-    id = Column(String,  primary_key=True, index=True, nullable=False)
+    #: Primary key (NOTE: this allows to rename a tag without breaking all existing relationships)
+    id = Column(GUID(), primary_key=True, index=True, default=uuid4)
+
+    #: The name of the tag
+    name = Column(String,  primary_key=True, index=True, nullable=False)
 
     def __repr__(self):
-        return f"<Tag '{self.id}'>"
+        return f"<Tag '{self.name}' (ID: {self.id})>"
+
+    @classmethod
+    def get_by_name(cls, session: Session, name: str) -> Optional[Any]:
+        """
+        Returns the tag corresponding to the given name.
+
+        :param session: the session (see flashcards_core.database:init_db()).
+        :param name: the name of the tag to return.
+        :returns: the matching tag object.
+        """
+        return session.query(cls).filter(cls.name == name).first()
+
+    @classmethod
+    async def get_by_name_async(cls, session: Session, name: str) -> Optional[Any]:
+        """
+        Returns the tag corresponding to the given name (asyncio-friendly).
+
+        :param session: the session (see flashcards_core.database:init_db()).
+        :param name: the name of the tag to return.
+        :returns: the matching tag object.
+        """
+        stmt = select(Tag).where(Tag.name == name)
+        results = await session.execute(stmt)
+        return results.first()
